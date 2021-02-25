34bc9afb8ef17ac3feb8a63439a89bb421f8847c
baignauman <baignauman@hotmail.com>
2021-02-25 09:33:49 +0100

    hello world update zigbee

--- zigbee_api.py
+++ zigbee_api.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import threading
 import flask
+from flask import render_template, request, url_for, flash, redirect
 import json
 import time
 global message
@@ -21,6 +22,28 @@ def thread_subscribe(client, topic_lst):
     client.loop_forever()
 
 
+@app.route('/', methods=('GET', 'POST'))
+def main():
+    if request.method == 'POST':
+        app.config['switch'] = request.form['switch']
+        app.config['brightness'] = request.form['brightness']
+        if request.form['switch'] == '':
+            return render_template('main.html')
+        if request.form['switch'] =='1':
+           return redirect(url_for('on'))
+        if request.form['switch'] =='0':
+           return redirect(url_for('off'))
+        else:
+            return render_template('main.html')
+    if request.method == 'GET':
+        try:
+            app.config['state']
+            app.config['brightness']
+            return render_template('main.html', state=app.config['state'], brightness=app.config['brightness'])
+        except:
+            return render_template('main.html')
+
+
 @app.route('/on', methods=['GET'])
 def on():
     client = mqtt.Client()
@@ -31,14 +54,19 @@ def on():
     d.setDaemon(True)
     d.start()
 #---------------------------
+    if app.config['brightness'] == '':
+        app.config['brightness'] = '100'
     client.publish("zigbee2mqtt/bulb/set", '{"state":"ON"}')
-    #client.publish("zigbee2mqtt/bulb/set", '{"brightness":"150"}')
+    client.publish("zigbee2mqtt/bulb/set", json.dumps({"brightness":app.config['brightness']}))
     global message
     try:
         time.sleep(0.1)
         message
         message_dict = json.loads(message.decode())
-        return "<h1>State: {}</p>".format(message_dict['state'])
+        app.config['state'] = message_dict['state']
+        app.config['brightness'] = message_dict['brightness']
+        
+        return redirect(url_for('main'))
     except:
         return "<h1>No feedback</p>"
         pass
@@ -59,10 +87,14 @@ def off():
         time.sleep(0.1)
         message
         message_dict = json.loads(message.decode())
-        return "<h1>State: {}</p>".format(message_dict['state'])
+        app.config['state'] = message_dict['state']
+        app.config['brightness'] = message_dict['brightness']
+        return redirect(url_for('main'))
     except:
         return "<h1>No feedback</p>"
         pass
+
+
 
 
 if __name__ == "__main__":
